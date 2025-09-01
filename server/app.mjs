f86342b608ec90d6061b5adbcd68cb34efded2f0
Pon Thanat <aaponaa2632@gmail.com>
2025-09-01 12:31:33 +0700

    add: add codes to assignment

--- server/app.mjs
+++ server/app.mjs
@@ -1,11 +1,42 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
+app.use(express.json());
 
 app.get("/test", (req, res) => {
   return res.json("Server API is working 🚀");
 });
+
+app.post("/assignments", async(req, res) => {
+  try {
+    const newAssignment = {
+      ...req.body,
+      created_at: new Date().toISOString(),
+      updated_at: new Date().toISOString(),
+      published_at: new Date().toISOString(),
+    };
+
+    await connectionPool.query(
+      `insert into assignments ( title, content, category)
+      values ($1, $2, $3)`,
+      [
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+      ]
+    );
+    return res.json({
+      message: "Created assignment sucessfully",
+    });
+
+ } catch (e) {
+   return res.json({
+     message: "Server could not create assignment because there are missing data from client",
+   });
+ }
+})
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
